f1a0e60f6d14eeb53b05dc40a2f40c6e77c7a521
vidhyasagar <thirumav@uci.edu>
2017-04-30 21:27:46 -0700

    new method

--- parallel-mergesort.cc
+++ parallel-mergesort.cc
@@ -9,54 +9,123 @@
 #include <stdlib.h>
 
 #include "sort.hh"
-void merge(keytype* a,int p,int q,int r){
+
+int binarySearch(keytype* b,keytype searchTerm,int startIndex,int endIndex){
+    int low = startIndex;
+    int high = MAX(startIndex,endIndex+1);
+    int mid;
+    while(low<high){
+          mid = low + (high-low)/2;
+          if(b[mid]>=searchTerm){
+            high = mid;
+          }
+          else if(b[mid]<searchTerm){
+            low = mid+1;
+          }
+        }
+      return high;
+}
+void merge(keytype* a,keytype *sorted,int p,int q,int w,int r,int copyStart){
 
 int size1 = q-p+1;
-int size2 = r-q;
+int size2 = r-w+1;
 keytype* temp1 = newKeys (size1);
 keytype* temp2 = newKeys (size2);
 // printf("p is,%d\n",p);
 // printf("q is,%d\n",q);
 // printf("r is %d\n",r);
+// printf("w is %d\n",w);
+// printf("merge\n");
 
 memcpy (temp1, a+p, size1 * sizeof (keytype));
-memcpy (temp2, a+q+1, size1 * sizeof (keytype));
+memcpy (temp2, a+w, size2 * sizeof (keytype));
 
 int m = 0;
 int n = 0;
-int index = p;
+int index = copyStart;
 
 while(m<size1 && n<size2){
       if(temp1[m]<temp2[n]){
-        a[index] = temp1[m];
+        sorted[index] = temp1[m];
         m++;
       }
       else{
-        a[index] = temp2[n];
+        sorted[index] = temp2[n];
         n++;
       }
       index++;
   }
   while(m<size1)
-    a[index++] = temp1[m++];
+    sorted[index++] = temp1[m++];
 
   while(n<size2)
-    a[index++] = temp2[n++];
+    sorted[index++] = temp2[n++];
 
 }
 
-void mergeSort(keytype* a,int left,int right){
+
+void paMerge(keytype *a,keytype *sorted,int aStart,int aEnd,int bStart,int bEnd,int copy){
+
+	int asize = aEnd-aStart+1;
+  int bsize = bEnd-bStart+1;
+  int size = asize+bsize;
+
+  if(size<=3){
+		merge(a,sorted,aStart,aEnd,bStart,bEnd,copy);
+	}
+	else{
+
+						if(asize<bsize){
+							int exchange ;
+
+							exchange = aStart;
+							aStart = bStart;
+							bStart = exchange;
+
+							exchange = aEnd;
+							aEnd = bEnd;
+							bEnd = exchange;
+
+							exchange = asize;
+							asize = bsize;
+							bsize = exchange;
+
+						}
+
+						if(asize == 0){
+							return;
+						}
+					  else{
+												int searchIndex = (aStart+aEnd)/2;
+											  keytype searchTerm = a[searchIndex];
+											  // printf ("Search term %lu\n",searchTerm);
+											  int foundIndex = binarySearch(a,searchTerm,bStart,bEnd);
+												int copyAt = copy+(searchIndex-aStart)+(foundIndex-bStart)+1;
+												sorted[copyAt-1] = searchTerm;
+											  paMerge(a,sorted,aStart,searchIndex-1,bStart,foundIndex-1,copy);
+											  paMerge(a,sorted,searchIndex+1,aEnd,foundIndex,bEnd,copyAt);
+
+								}
+	}
+
+
+}
+
+void mergeSort(keytype* a,keytype* temp,int left,int right){
 
 int mid ;
 
 if(left<right){
 	mid = left+(right-left)/2;
   // printf("mid,%d\n",mid);
-	mergeSort(a,left,mid);
-	mergeSort(a,mid+1,right);
-	merge(a,left,mid,right);
+	mergeSort(a,temp,left,mid);
+	mergeSort(a,temp,mid+1,right);
+	// merge(a,left,mid,mid+1,right);
+  paMerge(a,temp,left,mid,mid+1,right,left);
+	memcpy(a + left, temp + left, (right - left + 1) * sizeof(keytype));
   }
 }
+
 void
 parallelSort (int N, keytype* A)
 {
